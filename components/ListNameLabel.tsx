2dd17dcdaccbdb2d5666241330d89898ec9b2686
phihash <insharp0220@gmail.com>
2026-09-13 23:53:38 +0900

    微修正

--- components/ListNameLabel.tsx
+++ components/ListNameLabel.tsx
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { IconName } from "../types";
 
 type ListNameLabelProps = {
   color?: string;
+  listname?: string;
+  iconName?: IconName;
 };
 
-export default function ListNameLabel({ color }: ListNameLabelProps) {
+export default function ListNameLabel({
+  color,
+  listname,
+  iconName,
+}: ListNameLabelProps) {
   return (
     <View style={styles.chip}>
-      <View style={(styles.square, color && { backgroundColor: color })} />
-      <Text style={styles.name}>ラベル</Text>
+      <View style={[styles.square, color && { backgroundColor: color }]} />
+      <Text style={styles.name}>{listname ?? "ラベル"}</Text>
+      <Text>
+        {iconName && <Ionicons name={iconName} size={20} color="#555" />}
+      </Text>
     </View>
   );
 }
