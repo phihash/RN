61c2f7a73dbf5ae859fa32a8ae318deebf6d88d9
phihash <insharp0220@gmail.com>
2026-09-15 01:13:50 +0900

    タブUI

--- app/menu.tsx
+++ app/menu.tsx
@@ -1,17 +1,24 @@
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View, Text } from "react-native";
 import ItemRow from "../components/ItemRow";
 import { defaultItems } from "../data";
+import { useState } from "react";
+import { CATEGORIES } from "../types";
 
 export default function Menu() {
+  const [selectTab, setSelectTab] = useState("化粧品");
   return (
     <View style={styles.container}>
+      <View style={styles.tab}>
+        {CATEGORIES.map((item) => {
+          return <Text>{item}</Text>;
+        })}
+      </View>
+
       <FlatList
         data={defaultItems}
         keyExtractor={(item) => item.id}
         contentContainerStyle={styles.grid}
-        renderItem={({ item }) => (
-          <ItemRow name={item.name} icon={item.icon} />
-        )}
+        renderItem={({ item }) => <ItemRow name={item.name} icon={item.icon} />}
       />
     </View>
   );
@@ -21,6 +28,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  tab: {
+    flexDirection: "row",
+    padding: 16,
   },
   grid: {
     padding: 16,
